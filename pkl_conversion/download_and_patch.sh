fc4b1a70a060a1eb7c65c9bf6809b3f7521b9e49
Kirill <opcheese@gmail.com>
2020-06-23 13:28:48 +0500

    minor fixes

--- pkl_conversion/download_and_patch.sh
+++ pkl_conversion/download_and_patch.sh
@@ -1,13 +1,15 @@
 apt-get update
 apt-get -y install git
+apt-get -y install python3-pip
 pip3 install -r requirements.txt
 git clone https://github.com/NVlabs/stylegan2.git
 cd stylegan2
+pip3 install -r requirements.txt
 git checkout 4874628c7dfffaae01f89558c476842b475f54d5
 cp ../run_conversion.py .
-python run_conversion.py ../$1
+python3 run_conversion.py ../$1
 git apply ../patch.diff
-python run_training.py
+python3 run_training.py
 freeze_graph --input_saved_model_dir=saved_graph --output_graph=out.pb --output_node_names=Gs/G_synthesis/output
 cp out.pb ../
 cd ..
--- pkl_conversion/run_with_docker.sh
+++ pkl_conversion/run_with_docker.sh
@@ -1,1 +1,1 @@
-docker run --gpus all -v ${PWD}:/tmp/work tensorflow/tensorflow:1.15.0-gpu-py3 /bin/bash -c "cd /tmp/work; ./download_and_patch.sh $1 $2"
+docker run  -v ${PWD}:/tmp/work tensorflow/tensorflow:1.14.0 /bin/bash -c "cd /tmp/work; ./download_and_patch.sh $1 $2"
